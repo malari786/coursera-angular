6ecde34114687140415ec3395f6c627f78a42dc5
malari786 <malari@gmail.com>
2016-09-17 21:10:38 -0500

    Add files via upload

---
+++ module1_solution/app.js
@@ -0,0 +1,36 @@
+(function(){
+'use strict'
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$inject = ['$scope','$filter','$window'];
+function LunchCheckController($scope,$filter,$window){
+  $scope.lunchItems='';
+  $scope.message='';
+  $scope.upper= function(){
+  var upCase= $filter('uppercase');
+  $scope.name=upCase($scope.lunchItems);
+
+
+    };
+    $scope.checkIfTooMuch= function(){
+      if ($scope.lunchItems=='' || $scope.lunchItems=='undefined') {
+        $scope.message='Please Enter Data';
+        //$window.alert('Please Enter Data');
+
+  }
+  else {
+    var count = $scope.lunchItems.split(',');
+    if(count.length<=3){
+      $scope.message='Enjoy!'
+      //$window.alert(count.length);
+    }
+    else {
+      $scope.message='Too much!'
+      //$window.alert('Too much');
+    }
+  }
+
+};
+}
+
+})();
